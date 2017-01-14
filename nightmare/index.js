c825564e88a1703b4f0660b3e2ad744643659065
mejan <najemes0@gmail.com>
2017-01-15 00:11:55 +0100

    update comments

--- nightmare/index.js
+++ nightmare/index.js
@@ -1,8 +1,3 @@
-/*
-	Reading file for credentials (not working and fs is not installed)
-*/
-
-
 var loginUrl="https://portal.miun.se/c/portal/login?p_l_id=1060514&_miunRedirect=https%3A%2F%2Fportal.miun.se%2Fgroup%2Fstudent";
 var schemaUrl="https://portal.miun.se/group/student/my-schedule";
 
@@ -10,12 +5,12 @@ var Nightmare = require('nightmare');
 
 var grpRoom = new Nightmare()
 	/*
-		Settings
+         * Settings
 	*/
 	.viewport(1000,1000)
 	.useragent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
 	/*
-		Go to the bok group room page.
+         * Go to the bok group room page.
 	*/
 	.goto(loginUrl)
 	.wait(1000)
@@ -33,7 +28,7 @@ var grpRoom = new Nightmare()
 	.select("select[class='aui-field-select']",'svl') // choice campus.
 	.wait(3000)
 	/*
-		Change date below. 
+         * Change date below.
 	*/
 	.inject('js', 'jQuery.js')
 	.evaluate(function(){
@@ -47,7 +42,7 @@ var grpRoom = new Nightmare()
 	})
 	.wait(2000)
 	/*
-		pick and select room below
+         * pick and select room below
 	*/
 	.evaluate(function(){
 		//Select all the "time intervals" for room.
@@ -58,8 +53,12 @@ var grpRoom = new Nightmare()
 		for(var i = 0; i < elemArr.length; i++){
 			elemArr[i].className += " selected"
 		}
-		/*BELOW IS THE PROBLEM (buttom clicking), if this is fixed automated study room booking will work.*/
-		//Try to click comfire button (All the methodes below but somehow not in nightmare works in the browser console).
+		/*
+                 *
+                 * BELOW IS THE PROBLEM (buttom clicking), if this is fixed automated study room booking will work.
+                 * All the "clicking" in the comments below (until end of file) works in the browser, but not in nightmare somehow.
+                 *
+                 */
 		//$("button:contains('Utför bokning')").click(); // More suggestions for clicking button is below (comment out).
 		//document.getElementsByTagName("button")[1].click();
 		//document.getElementsByTagName("button")[1].setAttribute("data-menu", "doItLikaALady");
