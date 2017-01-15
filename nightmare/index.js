4620737dbd885f9cd0da4865014f130581a56e7e
mejan <najemes0@gmail.com>
2017-01-15 01:00:00 +0100

    new commentes and tries witgh no success

--- nightmare/index.js
+++ nightmare/index.js
@@ -5,12 +5,12 @@ var Nightmare = require('nightmare');
 
 var grpRoom = new Nightmare()
 	/*
-         * Settings
+	* Settings
 	*/
 	.viewport(1000,1000)
 	.useragent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
 	/*
-         * Go to the bok group room page.
+	* Go to the bok group room page.
 	*/
 	.goto(loginUrl)
 	.wait(1000)
@@ -28,7 +28,7 @@ var grpRoom = new Nightmare()
 	.select("select[class='aui-field-select']",'svl') // choice campus.
 	.wait(3000)
 	/*
-         * Change date below.
+	* Change date below.
 	*/
 	.inject('js', 'jQuery.js')
 	.evaluate(function(){
@@ -42,35 +42,26 @@ var grpRoom = new Nightmare()
 	})
 	.wait(2000)
 	/*
-         * pick and select room below
+	* pick and select room below
 	*/
 	.evaluate(function(){
 		//Select all the "time intervals" for room.
-		var elemArr = document.getElementById("L210").getElementsByTagName("td");
-		document.getElementById("resRoom").innerHTML = "L210";
-		document.getElementById("resStartTime").innerHTML = "08:00";
-		document.getElementById("resEndTime").innerHTML = "19:00";
+		var elemArr = document.getElementById("M101").getElementsByTagName("td");
 		for(var i = 0; i < elemArr.length; i++){
 			elemArr[i].className += " selected"
 		}
+		document.getElementById("resRoom").innerHTML = "M101";
+		document.getElementById("resStartTime").innerHTML = "08:00";
+		document.getElementById("resEndTime").innerHTML = "19:00";
 		/*
-                 *
-                 * BELOW IS THE PROBLEM (buttom clicking), if this is fixed automated study room booking will work.
-                 * All the "clicking" in the comments below (until end of file) works in the browser, but not in nightmare somehow.
-                 *
-                 */
+		*
+		* BELOW IS THE PROBLEM (buttom clicking), if this is fixed automated study room booking will work.
+		* All the "clicking" in the comments below (until end of file) works in the browser, but not in nightmare somehow.
+		*
+		*/
 		//$("button:contains('Utför bokning')").click(); // More suggestions for clicking button is below (comment out).
 		//document.getElementsByTagName("button")[1].click();
 		//document.getElementsByTagName("button")[1].setAttribute("data-menu", "doItLikaALady");
-	})
-	//.click("button[data-menu='doItLikaALady']")
-	.wait(1000)
-	.screenshot("progresspicture.png")
-	.run(function(){console.log("Done!")})
-	.end();
-
-
-
 		/*var bTags = document.getElementsByTagName("button");
 		var searchText = "Utför bokning";
 		for (var i = 0; i < bTags.length; i++) {
@@ -79,27 +70,7 @@ var grpRoom = new Nightmare()
 				break;
 			}
 		}*/
-
-
-	/*var bTags = document.getElementsByTagName("button");
-		var searchText = "Utför bokning";
-var testingShit=0;
-		for (var i = 0; i < bTags.length; i++) {
-			if (bTags[i].innerHTML == searchText) {
-				testingShit=i
-				bTags[i].setAttribute("data-menu", "clickMe");
-				break;
-			}
-		}*/
-	/*document.querySelectorAll("a[href='/group/student/my-schedule']").click();*/
-		//setTimeout(function(){}, 500);
-		//sleep(2000);
-
-		//setTimeout(function(){}, 1500);
-		//sleep(2000);
-	/*.wait(2000)
-	.evaluate(function(){
-		//$("button:contains('Utför bokning')").click();
+		/*
 		var bTags = document.getElementsByTagName("button");
 		var searchText = "Utför bokning";
 
@@ -109,5 +80,10 @@ var testingShit=0;
 				break;
 			}
 		}
-	})*/
-	//var bTags = document.getElementsByTagName("button");var searchText = "Utför bokning";var testingShit=0;for (var i = 0; i < bTags.length; i++) {if (bTags[i].innerHTML == searchText) {testingShit=i;break;}}
+		*/
+	})
+	//.click("button[data-menu='doItLikaALady']")
+	.wait(10000)
+	.screenshot("progresspicture.png")
+	.run(function(){console.log("Done!")})
+	.end();
